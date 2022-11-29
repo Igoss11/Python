1ed525ea4bdbb5790cf67fc26ce076d12d90c94d
igoss11 <igoss11@gmail.com>
2022-11-29 05:20:20 +0300

    finish task 2_5

--- sem2/task2_5.py
+++ sem2/task2_5.py
@@ -1,2 +1,11 @@
 # Реализуйте алгоритм перемешивания списка.
 
+start_list = list(range(-10, 10))
+print(start_list)
+
+for i in range(round(len(start_list)/2)):
+    if i % 2 == 0:
+        final_list = start_list[i]
+        start_list[i] = start_list[- i]
+        start_list[- i] = final_list
+print(start_list)
