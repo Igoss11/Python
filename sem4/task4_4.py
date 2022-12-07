08d6943566f3aacaa92a0957660b66228d9c7471
igoss11 <igoss11@gmail.com>
2022-12-08 02:43:59 +0300

    finish task 4_4

--- sem4/task4_4.py
+++ sem4/task4_4.py
@@ -2,3 +2,24 @@
 # список коэффициентов (значения от 0 до 100) многочлена
 # и записать в файл многочлен степени k.(записать в строку)
 
+from random import randint
+
+k = int(input('Введите натуральную степень k: '))
+polynomial = ''
+degree = k
+
+while degree >= 0:
+    num = randint(0, 100)
+    if degree > 1:
+        polynomial += f'{num}x^{degree}'
+    elif degree == 1:
+        polynomial += f'{num}x'
+    elif degree == 0:
+        polynomial += f'{num} = 0'
+    if degree > 0:
+        polynomial += ' + '
+    degree -= 1
+
+f = open('file.txt', 'w')
+f.write(str(polynomial))
+f.close()
