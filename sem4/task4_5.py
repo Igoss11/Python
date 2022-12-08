b030d7223e655302af32a44ccade5e2488efc0ca
igoss11 <igoss11@gmail.com>
2022-12-08 03:00:58 +0300

    finish task 4_5 add

--- sem4/task4_5.py
+++ sem4/task4_5.py
@@ -7,3 +7,19 @@
 # Результирующий файл:
 # 6*x**2 + 5*x + 9
 
+import sympy
+
+x = sympy.symbols('x')
+
+with open('file1.txt', 'r') as data:
+    polynomial1 = data.read()
+with open('file2.txt', 'r') as data:
+    polynomial2 = data.read()
+
+polynomial = (sympy.simplify((polynomial1 + '+' + polynomial2)))
+print(polynomial1)
+print(polynomial2)
+print('Результирующий многочлен: ', polynomial)
+
+with open("file_itog.txt", "w") as data:
+    data.write("%s" % polynomial)
